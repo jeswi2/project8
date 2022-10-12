1115d453b5cb7c9e16a9c5dd0f8f3bd25b690d29
jeswi2 <jeswimathewm@gmail.com>
2022-10-12 19:53:45 +0530

    vi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { ViewquotesComponent } from './viewquotes/viewquotes.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes:Routes=[
+  {
+    path:"",component:HomeComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    ViewquotesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
